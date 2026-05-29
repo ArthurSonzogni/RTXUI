4513a4353a730008fbd413649830c047fafd56c6
ArthurSonzogni <sonzogniarthur@gmail.com>
2026-05-29 22:03:21 +0200

    feat: implement horizontal rule (<hr>) component, add demo, test, and reference documentation

---
+++ example/hr.cpp
@@ -0,0 +1,73 @@
+// Copyright 2026 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+#include <iostream>
+#include <rtxui/rtxui.hpp>
+
+using namespace rtxui;
+
+class HrDemo : public Component<HrDemo> {
+ public:
+  std::string_view Setup() override {
+    Import<rtxui::div>();
+    Import<rtxui::span>();
+    Import<rtxui::hr>();
+    Import<rtxui::p>();
+    return R"html(
+      <div class="container">
+        <p class="title">Horizontal Rule Element Demo</p>
+        <p class="desc">The &lt;hr&gt; component renders a light box-drawing line horizontally, acting as a section separator.</p>
+        
+        <p class="section-title">Section 1: Introduction</p>
+        <p class="content">This is the first section of the document. Below it is a horizontal separator rule.</p>
+        
+        <hr />
+        
+        <p class="section-title">Section 2: Layout & Separation</p>
+        <p class="content">This is the second section of the document, neatly separated from the introduction above.</p>
+      </div>
+
+      <style>
+        self {
+          display: block;
+          padding: 1;
+          background-color: rgb(15, 23, 42);
+          color: white;
+          border: tall;
+          border-color: rgb(56, 189, 248);
+        }
+        .container {
+          display: block;
+        }
+        .title {
+          display: block;
+          font-weight: bold;
+          color: rgb(56, 189, 248);
+          margin-bottom: 1;
+        }
+        .desc {
+          display: block;
+          color: rgb(156, 163, 175);
+          margin-bottom: 2;
+        }
+        .section-title {
+          display: block;
+          font-weight: bold;
+          color: rgb(244, 63, 94);
+          margin-top: 1;
+        }
+        .content {
+          display: block;
+          color: rgb(229, 231, 235);
+        }
+      </style>
+    )html";
+  }
+};
+
+int main() {
+  auto app = Ref<HrDemo>::New();
+  Screen screen(app);
+  screen.Loop();
+  return 0;
+}
--- src/rtxui/component/default_components.cpp
+++ src/rtxui/component/default_components.cpp
@@ -1216,4 +1216,43 @@ bool option::OnEvent(Event event) {
   return false;
 }
 
+void hr::InitReflection() {
+  Bind(line_chars);
+  Component<hr>::InitReflection();
+}
+
+std::string_view hr::Setup() {
+  return R"html(<span class="hr-span">{line_chars}</span><style>
+    self {
+      display: block;
+      margin-top: 1;
+      margin-bottom: 1;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+    .hr-span {
+      color: #555;
+    }
+  </style>)html";
+}
+
+bool hr::Digest() {
+  auto* root = Root();
+  if (root) {
+    int layout_w = root->layout_width();
+    if (layout_w <= 0) {
+      layout_w = 80;
+    }
+    std::string new_line;
+    for (int i = 0; i < layout_w; ++i) {
+      new_line += "─";
+    }
+    if (line_chars != new_line) {
+      line_chars = new_line;
+      return true;
+    }
+  }
+  return Component<hr>::Digest();
+}
+
 }  // namespace rtxui
