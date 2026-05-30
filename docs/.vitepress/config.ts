a0279a7df275073b6cce1307c901596eddb7147a
ArthurSonzogni <sonzogniarthur@gmail.com>
2026-05-30 08:30:15 +0200

    docs: revert layout changes related to the three buttons

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,14 +7,7 @@ export default defineConfig({
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Tutorial', link: '/tutorial' },
-      {
-        text: 'Reference',
-        items: [
-          { text: 'C++ API', link: '/cpp_api' },
-          { text: 'HTML Elements', link: '/html_reference' },
-          { text: 'CSS Attributes', link: '/css_reference' }
-        ]
-      }
+      { text: 'Reference', link: '/cpp_api' }
     ],
     sidebar: [
       {
